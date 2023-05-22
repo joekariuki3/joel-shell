add295feafde8dd5647dc058de178ba0cb9522c3
joekariuki3 <joekariuki3@gmail.com>
2023-05-22 22:01:01 +0300

    Working on betty style
    getPath.c and exit.c

--- memoryManaging/exit.c
+++ memoryManaging/exit.c
@@ -1,4 +1,9 @@
 #include "shell.h"
+/**
+ * myexit - checks for exit command and terminates program
+ * @word: command to check
+ * Return: 1 if successful 0 if not
+ */
 int myexit(char *word)
 {
 	if (_strcmp(word, "exit") == 0)
--- memoryManaging/getPath.c
+++ memoryManaging/getPath.c
@@ -1,92 +1,71 @@
 #include "shell.h"
+/**
+ * getPath - gets PATH and looks for valid path to execute
+ * @wordArray: Array of comands entered
+ * @enVars: Array of enviros
+ * Return: null or valid path to execute
+ */
 char *getPath(char **wordArray, char **enVars)
 {
-    char *allPath = NULL, *tempPath = NULL, *path = NULL, *pathName = NULL;
-    int i = 0, pathNameSize = 0;
-    if (wordArray == NULL)
-    {
-        perror("WordArray NULL");
-        return (NULL);
-    }
+	char *allPath = NULL, *tempPath = NULL, *path = NULL, *pathName = NULL;
+	int i = 0, pathNameSize = 0;
 
-    else if (enVars == NULL)
-    {
-        perror("enVars NULL");
-        return (NULL);
-    }
-
-    /* loop through enVars vector to find the path variable */
-    for (i = 0; enVars[i] != NULL; i++)
-    {
-        /*look for PATH using strncmp*/
-        if (_strncmp(enVars[i], "PATH=", 5) == 0)
-        {
-            allPath = enVars[i] + 5; /*remove PATH=*/
-            break;
-        }
-    }
-    /* check if allPath was found */
-    if (allPath == NULL)
-    {
-        perror("allPath not found");
-        exit(1);
-    }
-
-    /* Make a copy of allPath n use the copy */
-    tempPath = _strdup(allPath);
-
-    path = strtok(tempPath, ":");
-
-    while (path)
-    {
-
-        if ((_strchr(wordArray[0], '/')) == NULL)
-        {
-            /* get the size of the complete pathName */
-            pathNameSize = _strlen(path) + _strlen("/") + _strlen(wordArray[0]);
-            /* allocate memory for the pathName */
-            pathName = (char *)malloc(pathNameSize * sizeof(char *));
-            if (pathName == NULL)
-            {
-                perror("Memory to PathName failed");
-                exit(1);
-            }
-            /* copy path to the pathname */
-            _strcpy(pathName, path);
-
-            /* add / to pathname */
-            _strcat(pathName, "/");
-
-            /* add the 1st argument to the path */
-            _strcat(pathName, wordArray[0]);
-        }
-        else
-        {
-            /* copy WordArray to PathName */
-            pathName = _strdup(wordArray[0]);
-        }
-
-        /* check if the full pathName is valid n executable */
-        if (access(pathName, X_OK) != 0)
-        {
-            /*free pathname*/
-            free(pathName);
-
-            /* move to next path*/
-            path = strtok(NULL, ":");
-        }
-        else
-        {
-            /* if path is valid*/
-            /*free tempPath allocated by _strdup()*/
-            free(tempPath);
-            /* return the valid path n exit.. it will be freed where it is being returned*/
-            return (pathName);
-        }
-    }
-    /* finish looping without getting valid path free tempPath allocated by _strdup()*/
-    free(tempPath);
-    /* print error*/
-    perror(wordArray[0]);
-    return (NULL);
+	if (wordArray == NULL || enVars == NULL)
+		return (NULL);
+	for (i = 0; enVars[i] != NULL; i++)/* loop through enVars PATH*/
+	{
+		if (_strncmp(enVars[i], "PATH=", 5) == 0)/*look for PATH using _strncmp*/
+		{
+			allPath = enVars[i] + 5;/*remove PATH=*/
+			break;
+		}
+	}
+	if (allPath == NULL)/* check if allPath was found */
+	{
+		perror("allPath not found");
+		exit(1);
+	}
+	tempPath = _strdup(allPath);/* Make a copy of allPath n use the copy */
+	path = strtok(tempPath, ":");
+	while (path)
+	{
+		if ((_strchr(wordArray[0], '/')) == NULL)
+		{
+			pathNameSize = _strlen(path) + _strlen("/")
+				+ _strlen(wordArray[0]);/* get the size of the complete pathName */
+			pathName = (char *)malloc(pathNameSize
+					* sizeof(char *));/* allocate memory for the pathName */
+			if (pathName == NULL)
+			{
+				perror("Memory to PathName failed");
+				exit(1);
+			}
+			_strcpy(pathName, path);/*copy path to the pathname*/
+			_strcat(pathName, "/");/* add / to pathname*/
+			_strcat(pathName, wordArray[0]);/* add the 1st argument to the path */
+		}
+		else
+			pathName = _strdup(wordArray[0]);/*copy WordArray to PathName */
+		if (access(pathName, X_OK) != 0)/*check pathName is valid n executable */
+		{
+			free(pathName);
+			path = strtok(NULL, ":");/* move to next path*/
+		}
+		else/* if path is valid*/
+		{
+			free(tempPath);/*free tempPath allocated by _strdup()*/
+			/*
+			 * return the valid path n exit..
+			 * it will be freed where it is being returned
+			 */
+			return (pathName);
+		}
+	}
+	/*
+	 * finish looping without getting valid path
+	 * free tempPath allocated by _strdup()
+	 */
+	free(tempPath);
+	perror(wordArray[0]);/* print error*/
+	return (NULL);
 }
